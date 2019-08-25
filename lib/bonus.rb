8b485f54525ff315ae4267888ec95707a519a52c
Ryan S <planetlucid@gmail.com>
2019-08-25 19:49:08 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,7 +20,8 @@ def bonus
    }
   }
 
-epic_tragedy[:montague||:capulet][:hero||:heroine][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
 
 puts epic_tragedy
   
